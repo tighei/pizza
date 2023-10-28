e30b8a243e15c606755dad2b0dc049d4d195ab39
tighei <oussama.adame12@gmail.com>
2023-10-28 14:00:37 +0100

    make swappin nav && make pointer to UP

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const btnUp = document.querySelector('.GoTop');
 const nav = document.getElementById('nav-bar');
-
 window.addEventListener('scroll', () => {
   if (window.scrollY !== 0) {
     btnUp.style.display = 'block';
@@ -15,3 +14,34 @@ btnUp.addEventListener('click', () => {
   btnUp.style.display = 'none';
   window.scrollTo(0, 0);
 });
+const homeLink = document.getElementById('home-link');
+const menuLink = document.getElementById('menu-link');
+const contactLink = document.getElementById('contact-link');
+const body = document.querySelector('body');
+const home = document.querySelector('.home');
+const menu = document.querySelector('.menu');
+const contact = document.querySelector('.contact');
+homeLink.addEventListener('click', () => {
+  hideAllSections();
+  home.classList.remove('hidden');
+  body.style.backgroundImage = 'url(/img/pizza-pepperoni-on-the-table.jpg)';
+});
+
+menuLink.addEventListener('click', () => {
+  hideAllSections();
+  menu.classList.remove('hidden');
+  body.style.backgroundImage = 'none';
+});
+
+contactLink.addEventListener('click', () => {
+  hideAllSections();
+  contact.classList.remove('hidden');
+  body.style.backgroundImage = 'none';
+});
+
+// Helper function to hide all sections
+function hideAllSections() {
+  home.classList.add('hidden');
+  menu.classList.add('hidden');
+  contact.classList.add('hidden');
+}
