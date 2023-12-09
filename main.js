242990f873bde87decc873a5247a08278c9c11f7
tighei <oussama.adame12@gmail.com>
2023-12-09 16:29:26 +0100

    Add font-weight to nav button and update  how to  swap between sections and clean code

--- main.js
+++ main.js
@@ -1,47 +1,45 @@
-const btnUp = document.querySelector('.GoTop');
-const nav = document.getElementById('nav-bar');
-window.addEventListener('scroll', () => {
-  if (window.scrollY !== 0) {
-    btnUp.style.display = 'block';
-    nav.style.backgroundColor = 'rgba(36, 36, 36,0.85)';
-  } else {
-    btnUp.style.display = 'none';
-    nav.style.backgroundColor = 'rgba(36, 36, 36,0.6)';
-  }
+const btnUp = document.querySelector(".GoTop");
+const nav = document.getElementById("nav-bar");
+window.addEventListener("scroll", () => {
+    if (window.scrollY !== 0) {
+        btnUp.style.display = "block";
+        nav.style.backgroundColor = "rgba(36, 36, 36,0.85)";
+    } else {
+        btnUp.style.display = "none";
+        nav.style.backgroundColor = "rgba(36, 36, 36,0.6)";
+    }
 });
 
-btnUp.addEventListener('click', () => {
-  btnUp.style.display = 'none';
-  window.scrollTo(0, 0);
-});
-const homeLink = document.getElementById('home-link');
-const menuLink = document.getElementById('menu-link');
-const contactLink = document.getElementById('contact-link');
-const body = document.querySelector('body');
-const home = document.querySelector('.home');
-const menu = document.querySelector('.menu');
-const contact = document.querySelector('.contact');
-homeLink.addEventListener('click', () => {
-  hideAllSections();
-  home.classList.remove('hidden');
-  body.style.backgroundImage = 'url(/img/pizza-pepperoni-on-the-table.jpg)';
+btnUp.addEventListener("click", () => {
+    btnUp.style.display = "none";
+    window.scrollTo(0, 0);
 });
 
-menuLink.addEventListener('click', () => {
-  hideAllSections();
-  menu.classList.remove('hidden');
-  body.style.backgroundImage = 'none';
-});
-
-contactLink.addEventListener('click', () => {
-  hideAllSections();
-  contact.classList.remove('hidden');
-  body.style.backgroundImage = 'none';
-});
-
-// Helper function to hide all sections
+const body = document.querySelector("body");
+let linked = document.querySelectorAll(".linked");
 function hideAllSections() {
-  home.classList.add('hidden');
-  menu.classList.add('hidden');
-  contact.classList.add('hidden');
+    const sections = document.querySelectorAll(".toggle-me");
+    sections.forEach((section) => {
+        section.classList.add("hidden");
+    });
 }
+
+linked.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        hideAllSections();
+
+        let className = e.target.classList[1];
+
+        let element = document.querySelector("#" + className);
+
+        if (element) {
+            element.classList.remove("hidden");
+            if (element.id === "home") {
+                body.style.backgroundImage =
+                    "url(/img/pizza-pepperoni-on-the-table.jpg)";
+            } else {
+                body.style.backgroundImage = "none";
+            }
+        }
+    });
+});
